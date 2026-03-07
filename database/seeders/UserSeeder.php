bc172d06d0d672937e920faef14b09becd69e624
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2026-03-07 18:29:14 +0800

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
@@ -18,8 +18,8 @@ class UserSeeder extends Seeder
         // Admin
         $admin = User::create([
             'name' => 'Administrator',
-            'email' => 'admin@bpk.com',
-            'password' => Hash::make('adminbpk123'),
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
             'role' => 'admin',
             'jenis_kelamin' => 'Laki-laki',
             'no_hp' => '081234567890',
@@ -33,26 +33,26 @@ class UserSeeder extends Seeder
 
         // Data relawan manusiawi
         $relawans = [
-            ['name' => 'Dewi Lestari', 'jenis_kelamin' => 'Perempuan', 'email' => 'dewi@bpk.com', 'jabatan' => 'Petugas Medis'],
-            ['name' => 'Wiza Pramana Putra', 'jenis_kelamin' => 'Laki-laki', 'email' => 'wiza@bpk.com', 'jabatan' => 'Petugas Teknik'],
-            ['name' => 'Siti Rahmawati', 'jenis_kelamin' => 'Perempuan', 'email' => 'siti@bpk.com', 'jabatan' => 'Petugas Lapangan'],
-            ['name' => 'Andi Nugroho', 'jenis_kelamin' => 'Laki-laki', 'email' => 'andi@bpk.com', 'jabatan' => 'Petugas Senior'],
-            ['name' => 'Aang Samudra', 'jenis_kelamin' => 'Laki-laki', 'email' => 'aang@bpk.com', 'jabatan' => 'Danton'],
-            ['name' => 'Lina Marlina', 'jenis_kelamin' => 'Perempuan', 'email' => 'lina@bpk.com', 'jabatan' => 'Petugas Medis'],
+            ['name' => 'Dewi Lestari', 'jenis_kelamin' => 'Perempuan', 'email' => 'dewi@gmail.com', 'jabatan' => 'Petugas Medis'],
+            ['name' => 'Wiza Pramana Putra', 'jenis_kelamin' => 'Laki-laki', 'email' => 'wiza@gmail.com', 'jabatan' => 'Petugas Teknik'],
+            ['name' => 'Siti Rahmawati', 'jenis_kelamin' => 'Perempuan', 'email' => 'siti@gmail.com', 'jabatan' => 'Petugas Lapangan'],
+            ['name' => 'Andi Nugroho', 'jenis_kelamin' => 'Laki-laki', 'email' => 'andi@gmail.com', 'jabatan' => 'Petugas Senior'],
+            ['name' => 'Aang Samudra', 'jenis_kelamin' => 'Laki-laki', 'email' => 'aang@gmail.com', 'jabatan' => 'Danton'],
+            ['name' => 'Lina Marlina', 'jenis_kelamin' => 'Perempuan', 'email' => 'lina@gmail.com', 'jabatan' => 'Petugas Medis'],
         ];
 
         foreach ($relawans as $index => $data) {
             $user = User::create([
                 'name' => $data['name'],
                 'email' => $data['email'],
-                'password' => Hash::make('relawanbpk123'),
+                'password' => Hash::make('password'),
                 'jenis_kelamin' => $data['jenis_kelamin'],
-                'no_hp' => '08' . rand(1000000000, 9999999999),
+                'no_hp' => '08'.rand(1000000000, 9999999999),
                 'tanggal_lahir' => now()->subYears(rand(22, 35))->format('Y-m-d'),
-                'alamat' => 'Jl. Relawan No.' . ($index + 1),
+                'alamat' => 'Jl. Relawan No.'.($index + 1),
                 'jabatan' => $data['jabatan'],
                 // 'jabatan' => collect(['Komandan','Wakil Komandan','Petugas Senior','Petugas Junior','Anggota Regu','Petugas Medis','Petugas Teknis','Petugas Lapangan','Danton'])->random(),
-                'golongan_darah' => collect(['A+','A-','B+','B-','AB+','AB-','O+','O-'])->random(),
+                'golongan_darah' => collect(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])->random(),
                 'status_aktif' => true,
             ]);
             $user->assignRole('relawan');
