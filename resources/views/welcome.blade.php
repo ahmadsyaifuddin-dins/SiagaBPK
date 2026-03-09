3cac32f43e2175f38c09252c4203a6a7dc9221ea
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2026-03-09 11:15:59 +0800

    sipp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -98,7 +98,7 @@
         </div>
     </nav>
 
-    <section id="beranda" class="relative pt-32 pb-20 lg:pt-48 lg:pb-32 overflow-hidden">
+    <section id="beranda" class="relative pt-28 pb-20 lg:pt-32 lg:pb-28 overflow-hidden">
         <div class="absolute inset-0 z-0">
             <div
                 class="absolute inset-0 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] opacity-50">
@@ -145,9 +145,9 @@
         </div>
     </section>
 
-    <section id="layanan" class="py-20 bg-white relative">
+    <section id="layanan" class="py-10 bg-white relative">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="text-center max-w-3xl mx-auto mb-16">
+            <div class="text-center max-w-3xl mx-auto mb-6">
                 <h2 class="text-3xl md:text-4xl font-bold text-gray-900 mb-4">Mengapa Menggunakan SiagaBPK?</h2>
                 <p class="text-gray-600 text-lg">Platform ini dirancang untuk mempermudah komunikasi antara masyarakat
                     dan petugas pemadam kebakaran demi meminimalisir kerugian.</p>
